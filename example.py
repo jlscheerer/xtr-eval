2bf005eb15b7d46b9cb9ad6a4e189aec9946fad8
Luca Scheerer <scheerer@future9.stanford.edu>
2024-08-06 10:42:27 -0700

    run xtr with lotte/lifestyle dataset

--- example.py
+++ example.py
@@ -1,5 +1,13 @@
+from xtr.config import XTRConfig, XTRModel, XTRScaNNIndexConfig
+from xtr.modeling.xtr import XTR
 from xtr.utils.lotte import load_lotte
+from xtr.utils.tracker import ExecutionTracker
 
 collection, queries, qas = load_lotte(dataset="lifestyle", datasplit="test", type_="search")
+config = XTRConfig(index_name="lotte", model=XTRModel.BASE_EN, index_config=XTRScaNNIndexConfig())
+xtr = XTR(config=config, collection=collection)
 
-print(collection, queries, qas)
+tracker = ExecutionTracker(name="XTR", steps=["Query Encoding", "Candidate Generation",
+                                              "Estimate Missing Similarity", "Aggregate Scores"])
+rankings = xtr.retrieve_docs(queries, token_top_k=1000, document_top_k=100, tracker=tracker)
+print(tracker.as_dict())
