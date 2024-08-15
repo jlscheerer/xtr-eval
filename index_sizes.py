961a1fe417d83b671ecd892f3868d3a0cf426351
Luca Scheerer <scheerer@future8.stanford.edu>
2024-08-14 23:13:08 -0700

    add index size calculation

---
+++ index_sizes.py
@@ -0,0 +1,48 @@
+import os
+
+from xtr.datasets import BEIR, BEIRDataset, LoTTE, LoTTEDataset
+from xtr.config import XTRConfig, XTRModel, XTRScaNNIndexConfig, XTRBruteForceIndexConfig, XTRFAISSIndexConfig, XTRIndexType
+from xtr.utils import xtr_tracker, canonical_index_name
+
+def filesize(path):
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    return sum(filesize(os.path.join(path, file)) for file in os.listdir(path))
+
+def index_size(dataset, index_config):
+    index_name = canonical_index_name(dataset=dataset, index_config=index_config)
+    config = XTRConfig(index_name=index_name, model=XTRModel.BASE_EN, index_config=index_config, override=False)
+
+    common_files = ["doc_offsets.npy", "tid2did.pickle"]
+    if index_config.index_type == XTRIndexType.BRUTE_FORCE:
+        specific_dir = "bruteforce"
+    elif index_config.index_type == XTRIndexType.FAISS:
+        specific_dir = "faiss"
+    elif index_config.index_type == XTRIndexType.SCANN:
+        specific_dir = "scann"
+    else: raise AssertionError
+    
+    total_size = 0
+    for entry in common_files + [specific_dir]:
+        total_size += filesize(os.path.join(config.path, entry))
+    return total_size
+
+def bytes_to_gib(size):
+    return size / (1024 * 1024 * 1024)
+
+DATASETS = [BEIRDataset(dataset=BEIR.SCIFACT, datasplit="test"),
+            BEIRDataset(dataset=BEIR.SCIDOCS, datasplit="test"),
+            BEIRDataset(dataset=BEIR.FIQA_2018, datasplit="test"),
+            LoTTEDataset(dataset=LoTTE.LIFESTYLE, datasplit="test"),
+            LoTTEDataset(dataset=LoTTE.TECHNOLOGY, datasplit="test"),
+            LoTTEDataset(dataset=LoTTE.SCIENCE, datasplit="test"),
+            LoTTEDataset(dataset=LoTTE.POOLED, datasplit="test"),]
+INDEX_CONFIGS = [XTRBruteForceIndexConfig(), XTRScaNNIndexConfig()]
+
+for dataset in DATASETS:
+    for index_config in INDEX_CONFIGS:
+        try:
+            size = bytes_to_gib(index_size(dataset, index_config))
+        except:
+            size = "-"
+        print(dataset.name, index_config.name, size, "GiB")
---
+++ xtr/experiments/__init__.py
@@ -0,0 +1,54 @@
+import os
+# Enforces CPU-only execution of torch
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# Configure environment to ensure single-threaded execution.
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"]= "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
+import torch
+torch.set_num_threads(1)
+
+from xtr.datasets import BEIR, BEIRDataset, LoTTE, LoTTEDataset
+from xtr.config import XTRConfig, XTRModel, XTRScaNNIndexConfig, XTRBruteForceIndexConfig, XTRFAISSIndexConfig
+from xtr.utils import xtr_tracker, canonical_index_name
+from xtr.modeling.xtr import XTR
+
+import json
+from datetime import datetime
+
+NUM_RUNS_PER_EXPERIMENT = 3
+
+def current_time_str():
+    return datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+def xtr_eval_latency(dataset, index_config, document_top_k, token_top_k):
+    index_name = canonical_index_name(dataset=dataset, index_config=index_config)
+    config = XTRConfig(index_name=index_name, model=XTRModel.BASE_EN, index_config=index_config, override=False)
+    xtr = XTR(config=config, collection=dataset.collection, device=torch.device("cpu"))
+    tracker = xtr_tracker(name=index_name)
+    rankings = xtr.retrieve_docs(dataset.queries, document_top_k=document_top_k, token_top_k=token_top_k, tracker=tracker)
+    return tracker, dataset.eval(rankings)
+
+def xtr_run_configuration(dataset, index_config, document_top_k, token_top_k):
+    tracker, metrics = xtr_eval_latency(dataset, index_config, document_top_k, token_top_k)
+    configuration = {"dataset": dataset.name, "index": index_config.name,
+                     "document_top_k": document_top_k, "token_top_k": token_top_k}
+    return {
+        "config": configuration,
+        "metrics": metrics,
+        "tracker": tracker.as_dict()
+    }
+
+def xtr_run_configurations(datasets, index_configs, document_top_k, token_top_k_values, label):
+    ctime = current_time_str()
+    os.makedirs("results", exist_ok=True)
+    filename = os.path.join("results", f"run_{label}_{ctime}.json")
+    results = []
+    for dataset in datasets:
+        for index_config in index_configs:
+            for token_top_k in token_top_k_values:
+                results.append(xtr_run_configuration(dataset, index_config, document_top_k=document_top_k, token_top_k=token_top_k))
+                with open(filename, "w") as file:
+                    json.dump(results, file)
