f96568e3ab7d8f08758e87db7e801a233c2112c4
Jan Luca Scheerer <lscheerer@ethz.ch>
2024-09-09 09:20:50 +0200

    bugfix in lotte eval (#4)
    Co-authored-by: Luca Scheerer <scheerer@future3.stanford.edu>

--- xtr/datasets/lotte.py
+++ xtr/datasets/lotte.py
@@ -46,32 +46,33 @@ class LoTTEDataset(Dataset):
         K_VALUES = [5, 10, 100, 1000]
         final_metrics = dict()
         for k in K_VALUES:
-            final_metrics[f"Success@{k}"] = _success_at_k_lotte(expected=expected, rankings=rankings, k=k)
-            final_metrics[f"Recall@{k}"] = _recall_at_k_lotte(expected=expected, rankings=rankings, k=k)
+            final_metrics[f"success@{k}"] = _success_at_k_lotte(expected=expected, rankings=rankings, k=k)
+            final_metrics[f"recall@{k}"] = _recall_at_k_lotte(expected=expected, rankings=rankings, k=k)
+        return final_metrics
 
 def _success_at_k_lotte(expected, rankings, k):
     num_total_qids, success = 0, 0
     for qid, answer_pids in expected.data.items():
         num_total_qids += 1
         if qid not in rankings.data:
-            print(f"WARNING: qid {qid} not found in {rankings}!", file=sys.stderr)
+            print(f"WARNING: qid {qid} not found in rankings!", file=sys.stderr)
             continue
         qid_rankings = set(map(lambda x: x[0], rankings.data[qid][:k]))
         if len(qid_rankings.intersection(answer_pids)) > 0:
                 success += 1
-    return success / num_total_qids * 100
+    return success / num_total_qids
 
 def _recall_at_k_lotte(expected, rankings, k):
     avg, num_relevant = 0, 0
     for qid, answer_pids in expected.data.items():
-        if str(qid) not in rankings.data:
-            print(f"WARNING: qid {qid} not found in {rankings}!", file=sys.stderr)
+        if qid not in rankings.data:
+            print(f"WARNING: qid {qid} not found in rankings!", file=sys.stderr)
             continue
         relevant_count = len(answer_pids)
         if relevant_count == 0:
             continue
         num_relevant += 1
-        qid_rankings = set(map(lambda x: x[0], rankings.data[str(qid)][:k]))
+        qid_rankings = set(map(lambda x: x[0], rankings.data[qid][:k]))
         correct_count = len(answer_pids & qid_rankings)
         avg += correct_count / relevant_count
     return avg / num_relevant
